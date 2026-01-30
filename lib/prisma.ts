c6c4adba8c4252d7937df6d607e8fdd5c00c06eb
Aidan McAlister <aidankmcalister@gmail.com>
2026-01-30 10:18:45 -0500

    update to prisma 7, updated to process proper response from create-db

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,18 @@
-import { PrismaClient } from "../app/generated/prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from '../app/generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+    prisma: PrismaClient
+}
 
-const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = globalForPrisma.prisma || new PrismaClient({
+  adapter,
+})
 
-export default prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
